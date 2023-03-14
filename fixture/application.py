79d30b8844c48793385c6fc449269ecb65385afd
Aziz Zulfikarov <aszulfikarov@npo-rit.ru>
2023-03-14 17:16:35 +0300

    работает с session

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 
+from fixture.session import SessionHelper
+
 
 class Application:
     def __init__(self):
@@ -9,10 +11,7 @@ class Application:
         self.wd = webdriver.Firefox(executable_path=r'C:\Users\Aziz\PycharmProjects\pythonProject2\geckodriver.exe',
                                     options=options)
         self.wd.implicitly_wait(30)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        self.session = SessionHelper(self)
 
     def return_to_groups_page(self):
         wd = self.wd
@@ -30,18 +29,6 @@ class Application:
     def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        self.open_groups_page()
 
     def open_home_page(self):
         wd = self.wd
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_group(Group(name="fggf", header="dsfdffd", footer="вавпр"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
