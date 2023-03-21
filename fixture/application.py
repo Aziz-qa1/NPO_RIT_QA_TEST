294c90a740c9371d95ce5c7101351f8547fe585c
Aziz Zulfikarov <aszulfikarov@npo-rit.ru>
2023-03-21 10:03:51 +0300

    Общий класс с фикстурой

--- fixture/application.py
+++ fixture/application.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
+from fixture.userfield import UserFieldsHelper
 
 
 class Application:
@@ -14,6 +15,7 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.user = UserFieldsHelper(self)
 
     def open_home_page(self):
         wd = self.wd
